68e875c09455edcda092e1a9de90337ef2df8583
goten4 <goten4@gmail.com>
2014-07-26 21:27:27 +0200

    Add Memory\Repository as invokable in service manager config

---
+++ config/module.config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'service_manager' => [
+        'invokables' => [
+            'GtnPersistBase\Infrastructure\Memory\Repository' => 'GtnPersistBase\Infrastructure\Memory\Repository',
+        ],
+    ],
+];
--- src/GtnPersistBase/Module.php
+++ src/GtnPersistBase/Module.php
@@ -4,10 +4,23 @@ namespace GtnPersistBase;
 use Zend\Loader\AutoloaderFactory;
 use Zend\Loader\StandardAutoloader;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
-class Module implements AutoloaderProviderInterface
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
     /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return include dirname(dirname(__DIR__)) . '/config/module.config.php';
+    }
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
      * @return array
      */
     public function getAutoloaderConfig()
